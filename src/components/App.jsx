b0d97c37c587df0409c6ba3293141864dcbc2264
Elvis <44731485+letlive419@users.noreply.github.com>
2023-09-24 18:17:11 -0400

    2nd commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { ReactDOM} from "react";
+import Weather from "./Weather";
 
+const api = process.env.REACT_APP_API_KEY;
+function handleClick(){
+    alert(api)
+}
 function App(){
-
+    
     return (
     <div>
-        <h1>
-            Hello World
-        </h1>
+        <button onClick={handleClick}> click me to show api</button>
+        <Weather />
     </div>
     )
 }
---
+++ src/components/Weather.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import axios from "axios";
+
+
+function Weather() {
+
+    
+//https://api.openweathermap.org/data/2.5/weather?q=miami&appid=17dadd5b9f978b59bc2d5be10a3ee822&units=imperial
+    return(
+        <div className="weather">
+            <h1> Weather area.</h1>
+        </div>
+    )
+
+}
+
+export default Weather;
