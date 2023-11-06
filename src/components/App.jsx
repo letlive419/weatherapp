359e827399012d27152c6fb1d67d4ccf18890091
Elvis <44731485+letlive419@users.noreply.github.com>
2023-11-05 19:45:30 -0500

    Nov5th 2023

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,12 @@ import { ReactDOM} from "react";
 import Weather from "./Weather";
 
 
-const api = process.env.REACT_APP_API_KEY;
-console.log(api)
+
 function App(){
     
     return (
     <div>
-        <h1> {api}</h1>
+        
        
         <Weather />
     </div>
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,17 +1,49 @@
 import React from "react";
 import axios from "axios";
-
+import { useState, useEffect } from "react";
 
 function Weather() {
+    const api = process.env.REACT_APP_API_KEY;
+    const [dataToShow, setDataToShow] = useState(null);
+    const [error, setError] = useState(null);
+    //axios method to get data
+    useEffect(() => {
+      // defining the function inside useEffect
+      const fetchData = async () => {
+        try {
+          const response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?q=miami&appid=${api}&units=imperial`
+          );
+          // handle success
+          setDataToShow(response.data.main.temp); // set state with the temperature data
+        } catch (error) {
+          // handle error
+          setError(error);
+          console.error("Error fetching data: ", error);
+        }
+      }
+      fetchData();
+    });
+       
+      
 
+      if (dataToShow === null && error === null) {
+        return <div>Loading...</div>;
+      }
     
-//https://api.openweathermap.org/data/2.5/weather?q=miami&appid=17dadd5b9f978b59bc2d5be10a3ee822&units=imperial
+      // Display error message if request failed
+      if (error) {
+        return <div>Error fetching data.</div>;
+      }
+    
+
     return(
         <div className="weather">
-            <h1> Weather area.</h1>
+            <h1> Here is the weather = {dataToShow} F </h1>
         </div>
     )
 
 }
+
 
 export default Weather;
